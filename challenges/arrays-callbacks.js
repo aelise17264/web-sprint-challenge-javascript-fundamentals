f6e19ddd7a19c726e86be152d0417da06b33e5c2
Anna Brander <aeilse17264@gmail.com>
2020-06-19 10:14:14 -0600

    completed MVP on arrays-callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -47,17 +47,24 @@ create a new array of objects called lowPopulationAnimals which contains only th
 with a population less than 5.
 
 */
-const lowPopulationAnimals
+const lowPopulationAnimals = zooAnimals.filter(function(item){
+  return item.population < 5;
+});
 
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. Remember the 
+reduce method takes two arguments: a callback (which itself takes two args), and an initial 
+value for the count.
 
 */
-//let populationTotal = 0;
-//console.log(populationTotal);
+let populationTotal = zooAnimals.reduce(function(acc, item) {
+return acc + item.population;
+},0);
+console.log(populationTotal);
 
 
 // ==== Callbacks ====  
@@ -68,21 +75,35 @@ The zoos need to know their total animal population across the United States. Fi
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume(a, b, cb){
+  return cb(a, b)
+}
 
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and returns 
+  * "Hello first-name last-name, nice to meet you!"
 */
-
+const add = function(a,b){
+  return a + b;
+}
+const multiply = function(a,b){
+  return a * b;
+}
+const greeting = function (first, last){
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
 // console.log(consume(10, 16, multiply)); // 160
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
-
+console.log(consume(2, 2, add));
+console.log(consume(10, 16, multiply));
+console.log(consume("Mary", "Poppins", greeting));
 
 
 /*
